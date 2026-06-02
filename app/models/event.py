f8c1a808a2cde4c3f2b13d2fea751fe01faab44a
Lorenzoop12 <l.pinna126@studenti.unica.it>
2026-06-02 19:51:15 +0200

    a

--- app/models/event.py
+++ app/models/event.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 class Event(SQLModel, table=True):
-    id : int = Field(primary_key=True)
+    id : int = Field(default = None, primary_key=True)
     title: str
     description: str
     date: datetime
