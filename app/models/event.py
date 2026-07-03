bc682e55bebd89e370fa099919e67fb8546b8b6d
Lorenzoop12 <l.pinna126@studenti.unica.it>
2026-07-03 15:00:33 +0200

    Updated code

--- app/models/event.py
+++ app/models/event.py
@@ -1,16 +1,17 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
+from typing import Annotated
 
 
 class EventBase(SQLModel):
-    title: str
-    description: str
+    title: Annotated[str, Field(min_length=1, max_length=30)]
+    description: Annotated[str, Field(min_length=1, max_length=200)]
     date: datetime
-    location: str
+    location: Annotated[str, Field(min_length=1, max_length=50)]
 
 
 class Event(EventBase, table=True):
-    id : int = Field(default = None, primary_key=True)
+    id : int = Field(default=None, primary_key=True)
 
 
 class EventCreate(EventBase):
