e7c1e83b652380b249e436bdf719802ab2b61b6d
Lorenzoop12 <l.pinna126@studenti.unica.it>
2026-07-03 11:23:34 +0200

    Updated models

--- app/models/event.py
+++ app/models/event.py
@@ -2,9 +2,20 @@ from sqlmodel import SQLModel, Field
 from datetime import datetime
 
 
-class Event(SQLModel, table=True):
-    id : int = Field(default = None, primary_key=True)
+class EventBase(SQLModel):
     title: str
     description: str
     date: datetime
     location: str
+
+
+class Event(SQLModel, table=True):
+    id : int = Field(default = None, primary_key=True)
+
+
+class EventCreate(EventBase):
+    pass
+
+
+class EventiPublic(EventBase):
+    id: int
