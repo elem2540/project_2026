d28edbee5659459a5d738da7ca7849d508c3142a
Lorenzoop12 <l.pinna126@studenti.unica.it>
2026-07-03 11:17:24 +0200

    Updated models

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,18 @@
 from sqlmodel import SQLModel, Field
 
 
-class User(SQLModel, table=True):
-    username: str = Field(primary_key=True)
+class UserBase(SQLModel):
     name: str
     email: str
+
+
+class User(SQLModel, table=True):
+    username: str = Field(default = None, primary_key=True)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserPublic(UserBase):
+    username: str
